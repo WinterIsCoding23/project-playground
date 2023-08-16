14295bc05c3507bd70bd31bde0ae99ed4aef843b
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-16 13:28:28 +0200

    replace number 0 with string 0 in sendNumberValue & create resetAll-function and eventListener to clear display

--- calculator/script.js
+++ calculator/script.js
@@ -7,7 +7,7 @@ function sendNumberValue(number) {
   // if current display value is 0, replace it, if not add number
   const displayValue = calculatorDisplay.textContent;
   calculatorDisplay.textContent =
-    displayValue === 0 ? number : displayValue + number;
+    displayValue === "0" ? number : displayValue + number;
 }
 
 // Add event-listeners for numbers, operators, decimal button
@@ -26,3 +26,11 @@ inputBtns.forEach((inputBtn) => {
     });
   }
 });
+
+// Reset Display
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+// Event Listener
+clearBtn.addEventListener("click", resetAll);
