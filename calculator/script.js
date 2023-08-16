8b13f8deefac7f6e159768b5da460db0e3f685fe
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-16 12:23:06 +0200

    declare sendNumberValue-function & inputBtns.forEach & fix function being called directly

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,25 @@
 const calculatorDisplay = document.querySelector("h1");
+// inputsBtns -> array of all buttons
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
+
+function sendNumberValue(number) {
+  console.log(number);
+}
+
+// Add event-listeners for numbers, operators, decimal button
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", function () {
+      sendNumberValue(inputBtn.value);
+    });
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", function () {
+      sendNumberValue(inputBtn.value);
+    });
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", function () {
+      sendNumberValue();
+    });
+  }
+});
