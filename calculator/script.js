b3f36a5e8137f3150d666d476254ba4588f1893d
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-16 13:36:31 +0200

    create addDecimal-function and call it via click-event on decimal-button

--- calculator/script.js
+++ calculator/script.js
@@ -10,6 +10,13 @@ function sendNumberValue(number) {
     displayValue === "0" ? number : displayValue + number;
 }
 
+function addDecimal() {
+  // if no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
 // Add event-listeners for numbers, operators, decimal button
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
@@ -22,7 +29,7 @@ inputBtns.forEach((inputBtn) => {
     });
   } else if (inputBtn.classList.contains("decimal")) {
     inputBtn.addEventListener("click", function () {
-      sendNumberValue();
+      addDecimal();
     });
   }
 });
