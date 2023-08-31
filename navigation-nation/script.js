f5ddf512c101aeb4cd77564ec86c858c9a7be029
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-31 10:23:57 +0200

    create event-listeners for toggleNav

--- navigation-nation/script.js
+++ navigation-nation/script.js
@@ -5,3 +5,11 @@ const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
+
+// Event listeners:
+menuBars.addEventListener("click", toggleNav);
+nav1.addEventListener("click", toggleNav);
+nav2.addEventListener("click", toggleNav);
+nav3.addEventListener("click", toggleNav);
+nav4.addEventListener("click", toggleNav);
+nav5.addEventListener("click", toggleNav);
